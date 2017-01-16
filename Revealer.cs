63539e3ad43c716f1a7af024a3a5bcfff09c35bd
viachpaliy <viachpaliy@ukr.net>
2017-01-17 00:15:57 +0200

    add Revealer class

---
+++ Revealer.cs
@@ -0,0 +1,102 @@
+﻿using GLib;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Gtk3
+{
+	public class Revealer : Bin
+	{
+		//
+		// Static Methods
+		//
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern IntPtr gtk_revealer_new ();
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern bool gtk_revealer_get_reveal_child (IntPtr raw);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern void gtk_revealer_set_reveal_child (IntPtr raw, bool reveal_child);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern bool gtk_revealer_get_child_revealed (IntPtr raw);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern uint gtk_revealer_get_transition_duration (IntPtr raw);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern void gtk_revealer_set_transition_duration (IntPtr raw, uint duration);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern int gtk_revealer_get_transition_type (IntPtr raw);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		private static extern void gtk_revealer_set_transition_type (IntPtr raw, int type);
+
+
+		//
+		//	Properties
+		//
+
+		[Property ("child-revealed")]
+		//Whether the child is revealed and the animation target reached.
+		public bool ChildRevealed {
+			get{
+				return Revealer.gtk_revealer_get_child_revealed (base.Handle);
+			}
+		}
+
+		[Property ("reveal-child")]
+		//Whether the container should reveal the child.
+		public bool RevealChild {
+			get {
+				return Revealer.gtk_revealer_get_reveal_child (base.Handle);
+			}
+			set{
+				Revealer.gtk_revealer_set_reveal_child (base.Handle, value);
+			}
+		}
+
+		[Property ("transition-duration")]
+		//The animation duration, in milliseconds.
+		public uint TransitionDuration {
+			get {
+				return Revealer.gtk_revealer_get_transition_duration (base.Handle);
+			}
+			set {
+				Revealer.gtk_revealer_set_transition_duration (base.Handle, value);
+			}
+		
+		}
+
+		[Property ("transition-type")]
+		//The type of animation used to transition.
+		public RevealerTransitionType TransitionType {
+			get {
+				return (RevealerTransitionType)Revealer.gtk_revealer_get_transition_type (base.Handle);
+			}
+			set {
+				Revealer.gtk_revealer_set_transition_type (base.Handle, (int)value);
+			}
+
+		}
+
+		//
+		// Constructors
+		//
+
+		//protected Revealer() : base(IntPtr.Zero){}
+
+		public Revealer(IntPtr raw) : base(IntPtr.Zero)
+		{
+			this.Raw = raw;
+		}
+
+		public Revealer() : base(IntPtr.Zero) 
+		{
+			this.Raw = Revealer.gtk_revealer_new ();
+		}
+
+	}
+
+}
---
+++ RevealerTransitionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gtk3
+{
+	public enum RevealerTransitionType
+	{
+		GTK_REVEALER_TRANSITION_TYPE_NONE,	//No transition
+
+		GTK_REVEALER_TRANSITION_TYPE_CROSSFADE,	//Fade in
+
+		GTK_REVEALER_TRANSITION_TYPE_SLIDE_RIGHT,	//Slide in from the left
+
+		GTK_REVEALER_TRANSITION_TYPE_SLIDE_LEFT,	//Slide in from the right
+
+		GTK_REVEALER_TRANSITION_TYPE_SLIDE_UP,	//	Slide in from the bottom
+
+		GTK_REVEALER_TRANSITION_TYPE_SLIDE_DOWN	//Slide in from the top
+	}
+}
+
