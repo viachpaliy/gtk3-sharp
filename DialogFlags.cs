a32bd3d84d9333f1e6f9ba28c69b966aa86371e8
viachpaliy <viachpaliy@ukr.net>
2017-06-21 23:57:44 +0300

    add DialogFlags.cs

---
+++ DialogFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gtk3
+{
+	public enum DialogFlags
+	{
+		Modal ,//GTK_DIALOG_MODAL	Make the constructed dialog modal
+
+		DestroyWithParent ,//GTK_DIALOG_DESTROY_WITH_PARENT
+		                        // Destroy the dialog when its parent is destroyed
+
+		UseHeaderBar //GTK_DIALOG_USE_HEADER_BAR
+				     //Create dialog with actions in header bar instead of action area. 
+	}
+}
+
