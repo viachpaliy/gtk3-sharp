30afc7dc3a4fc1da26c15f32b89e516b1d263f21
viachpaliy <viachpaliy@ukr.net>
2017-06-21 23:52:55 +0300

    add DialogFlags enum

--- DeleteType.cs
+++ DeleteType.cs
@@ -4,22 +4,22 @@ namespace Gtk3
 {
 	public enum DeleteType
 	{
-		GTK_DELETE_CHARS,	//Delete characters.
+		DeleteChars,	//Delete characters.
 
-		GTK_DELETE_WORD_ENDS,	//Delete only the portion of the word to the left/right of cursor if we’re in the middle of a word.
+		DeleteWordEnds,	//Delete only the portion of the word to the left/right of cursor if we’re in the middle of a word.
 
-		GTK_DELETE_WORDS,		//Delete words.
+		DeleteWords,		//Delete words.
 
-		GTK_DELETE_DISPLAY_LINES,	//Delete display-lines. Display-lines refers to the visible lines, with respect to to the current line breaks.
+		DeleteDisplayLines,	//Delete display-lines. Display-lines refers to the visible lines, with respect to to the current line breaks.
 									//As opposed to paragraphs, which are defined by line breaks in the input.
 
-		GTK_DELETE_DISPLAY_LINE_ENDS,	//Delete only the portion of the display-line to the left/right of cursor.
+		DeleteDisplayLineEnds,	//Delete only the portion of the display-line to the left/right of cursor.
 
-		GTK_DELETE_PARAGRAPH_ENDS,		//Delete to the end of the paragraph. Like C-k in Emacs (or its reverse).
+		DeleteParagraphEnds,		//Delete to the end of the paragraph. Like C-k in Emacs (or its reverse).
 
-		GTK_DELETE_PARAGRAPHS, 		//Delete entire line. Like C-k in pico.
+		DeleteParagraphs, 		//Delete entire line. Like C-k in pico.
 
-		GTK_DELETE_WHITESPACE     //Delete only whitespace. Like M-\ in Emacs.
+		DeleteWhitespace     //Delete only whitespace. Like M-\ in Emacs.
 	}
 }
 
