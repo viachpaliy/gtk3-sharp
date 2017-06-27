0ce203ef00ef6f086d36b53adfb40159f6b648eb
viachpaliy <viachpaliy@ukr.net>
2017-06-28 00:00:03 +0300

    add ShortcutsWindow class

--- Application.cs
+++ Application.cs
@@ -1,32 +1,59 @@
-﻿// Gtk3.Application.cs - GTK Main Event Loop class implementation
+﻿// Copyright (c) 2017 Author : Viacheslav Paliy
 //
-//This program is free software; you can redistribute it and/or
-// modify it under the terms of version 2 of the Lesser GNU General 
-// Public License as published by the Free Software Foundation.
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this program; if not, write to the
-// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-// Boston, MA 02111-1307, US
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Gdk;
+using GLib;
 
 namespace Gtk3 {
 
-	using System;
-	using System.Reflection;
-	using System.Runtime.InteropServices;
-	using System.Threading;
-	using Gdk;
-	using GLib;
+	/// <summary>
+	/// Application class.
+	/// </summary>
+	public  class Application {
 
-	public partial class Application {
+		#region Private Static Methods
 		
 		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
 		static extern void gtk_init (ref int argc, ref IntPtr argv);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		static extern void gtk_main ();
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		static extern void gtk_main_iteration ();
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		static extern bool gtk_main_iteration_do (bool blocking);
+
+		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
+		static extern void gtk_main_quit ();
+
+
+		#endregion
 
 		public static void Init ()
 		{
@@ -40,19 +67,13 @@ namespace Gtk3 {
 			//SynchronizationContext.SetSynchronizationContext (new GLib.GLibSynchronizationContext ());
 		}
 
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern void gtk_main ();
 
 		public static void Run ()
 		{
 			gtk_main ();
 		}
 
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern void gtk_main_iteration ();
-
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern bool gtk_main_iteration_do (bool blocking);
+	
 
 		public static void RunIteration ()
 		{
@@ -64,8 +85,7 @@ namespace Gtk3 {
 			return gtk_main_iteration_do (blocking);
 		}
 
-		[DllImport (Global.GtkNativeDll, CallingConvention = CallingConvention.Cdecl)]
-		static extern void gtk_main_quit ();
+	
 
 		public static void Quit ()
 		{
---
+++ ShortcutsWindow.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) 2017 Author : Viacheslav Paliy
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+using GLib;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Gtk3
+{
+	/// <summary>
+	///Toplevel which shows help for shortcuts.
+	/// </summary>
+	public class ShortcutsWindow : Window
+	{
+		#region Properties
+	
+		/// <summary>
+		/// The name of the section to show.
+		/// </summary>
+		[Property ("section-name")]
+		public string SectionName {
+			get {
+				GLib.Value property = base.GetProperty ("section-name");
+				string result = (string)property;
+				property.Dispose ();
+				return result;
+			}
+			set {
+				GLib.Value val = new GLib.Value (value);
+				base.SetProperty ("section-name", val);
+				val.Dispose ();
+			}
+		}
+
+
+
+		/// <summary>
+		/// The view name by which to filter the contents.
+		/// </summary>
+		[Property ("view-name")]
+		public string ViewName {
+			get {
+				GLib.Value property = base.GetProperty ("view-name");
+				string result = (string)property;
+				property.Dispose ();
+				return result;
+			}
+			set {
+				GLib.Value val = new GLib.Value (value);
+				base.SetProperty ("view-name", val);
+				val.Dispose ();
+			}
+		}
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// This event is raised when the user uses a keybinding to close the window.
+		/// </summary>
+		[Signal ("close")]
+		public event EventHandler Close {
+			add {
+				Signal signal = Signal.Lookup (this, "close");
+				signal.AddDelegate (value);
+			}
+			remove {
+				Signal signal = Signal.Lookup (this, "close");
+				signal.RemoveDelegate (value);
+			}
+		}
+
+		/// <summary>
+		/// This event is raised when the user uses a keybinding to start a search.
+		/// </summary>
+		[Signal ("search")]
+		public event EventHandler Search {
+			add {
+				Signal signal = Signal.Lookup (this, "search");
+				signal.AddDelegate (value);
+			}
+			remove {
+				Signal signal = Signal.Lookup (this, "search");
+				signal.RemoveDelegate (value);
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		///Internal constructor.Constructs the object from a C-based pointer.
+		/// </summary>
+		/// <param name="raw">The raw GObject reference associated with this object.</param>
+		public ShortcutsWindow (IntPtr raw) : base (raw)
+		{
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		protected ShortcutsWindow () : base (IntPtr.Zero){
+		}
+
+		#endregion
+
+
+	}
+}
+
